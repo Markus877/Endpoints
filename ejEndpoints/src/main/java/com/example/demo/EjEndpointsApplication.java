68031c0498bb704c1a9062d3d534ca3951ea6f1a
a940735 <a940735@10.234.58.240>
2025-04-02 16:22:47 +0200

    Proyecto terminado

---
+++ ejEndpoints/src/main/java/com/example/demo/EjEndpointsApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.model.Proyect;
+import com.example.service.ProjectService;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example")
+public class EjEndpointsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EjEndpointsApplication.class, args); }
+        @Bean
+        CommandLineRunner init(ProjectService projectService) {
+            return args -> {
+                projectService.createProject(new Proyect(1L, "Proyecto 1", "Java", true));
+                projectService.createProject(new Proyect(2L, "Proyecto 2", "Python", true));
+                projectService.createProject(new Proyect(3L, "Proyecto 3", "JavaScript", false));
+            };
+    }
+}
